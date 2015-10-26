9ac3bfcf996b35ba6ee9153d3526d1049dc57cc6
John Becker <john@beckersoft.com>
2015-10-26 08:48:01 -0400

    Attributed Authors

--- Freshdesk/CreateTicketRequest.cs
+++ Freshdesk/CreateTicketRequest.cs
@@ -3,6 +3,7 @@
  *
  * Author(s):
  *  John Becker (john@beckersoft.com)
+ *  Justin Maier (Github user: JustMaier)
  *  
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
